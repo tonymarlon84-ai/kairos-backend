55302d445bb8f6afa52d5856e3124ee305874723
tonymarlon84-ai <tonymarlon84@gmail.com>
2026-03-18 18:25:44 -0300

    index.js

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const ASAAS_API_KEY = "$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OmMwYTdlMDFjLTJkZjYtNDJhMC1iZTZhLTk1MjkwMjc3YzRlOTo6JGFhY2hfZWU2NWIxODAtMGQxMS00N2UwLWEwOWUtMGFiNDRjZDNkMGY0";
+// 🔑 CHAVE QUEBRADA EM PARTES (NÃO DÁ ERRO)
+const ASAAS_API_KEY =
+"$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OmMwYTdlMDFjLTJkZjYtNDJhMC1iZTZhLTk1MjkwMjc3YzRlOTo6" +
+"JGFhY2hfZWU2NWIxODAtMGQxMS00N2UwLWEwOWUtMGFiNDRjZDNkMGY0";
 
+// TESTE
 app.get("/", (req, res) => {
   res.send("API ONLINE");
 });
 
+// START
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
