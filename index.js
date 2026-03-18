3876582666e6c79ef1c61258eba5511fd5b25f65
tonymarlon84-ai <tonymarlon84@gmail.com>
2026-03-18 18:39:35 -0300

    index.js

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// 🔑 CHAVE QUEBRADA EM PARTES (NÃO DÁ ERRO)
-const ASAAS_API_KEY =
-"$aact_prod_000MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OmMwYTdlMDFjLTJkZjYtNDJhMC1iZTZhLTk1MjkwMjc3YzRlOTo6" +
-"JGFhY2hfZWU2NWIxODAtMGQxMS00N2UwLWEwOWUtMGFiNDRjZDNkMGY0";
+// 🔑 VEM DO RENDER (SEM QUEBRAR NADA)
+const ASAAS_API_KEY = process.env.ASAAS_API_KEY;
 
-// TESTE
 app.get("/", (req, res) => {
   res.send("API ONLINE");
 });
 
-// START
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
